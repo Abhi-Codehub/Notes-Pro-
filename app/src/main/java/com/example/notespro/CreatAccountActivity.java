19af901645e11ab3667160cec197b984ef009538
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-14 21:20:02 +0530

    Notes Pro(3)

--- app/src/main/java/com/example/notespro/CreatAccountActivity.java
+++ app/src/main/java/com/example/notespro/CreatAccountActivity.java
@@ -3,12 +3,35 @@ package com.example.notespro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class CreatAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_account);
+
+        emailEditText  = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_button);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view);
+
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        loginBtnTextView.setOnClickListener(v-> finish());
+    }
+
+    void createAccount(){
+
     }
 }

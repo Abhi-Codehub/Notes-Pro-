065368fcca89104f402637794ea4a89f588586ee
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-15 15:14:29 +0530

    Notes Pro Adding notes to the firebase

--- app/src/main/java/com/example/notespro/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notespro/NoteDetailsActivity.java
@@ -1,10 +1,16 @@
 package com.example.notespro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetailsActivity extends AppCompatActivity {
     EditText titleEditText, contentEditText;
@@ -30,6 +36,30 @@ public class NoteDetailsActivity extends AppCompatActivity {
             titleEditText.setError("Title is required");
             return;
         }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    void saveNoteToFirebase(Note note) {
+        DocumentReference documentReference;
+
+            documentReference = Utility.getCollectionReferenceForNotes().document();
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is added
+                    Utility.showToast(NoteDetailsActivity.this,"Note added successfully");
+                    finish();
+                }else{
+                    Utility.showToast(NoteDetailsActivity.this,"Failed while adding note");
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/notespro/Utility.java
+++ app/src/main/java/com/example/notespro/Utility.java
@@ -3,9 +3,18 @@ package com.example.notespro;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Utility {
     static void showToast(Context context, String message){
         Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
     }
-
+    static CollectionReference getCollectionReferenceForNotes(){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return FirebaseFirestore.getInstance().collection("notes")
+                .document(currentUser.getUid()).collection("my_notes");
+    }
 }

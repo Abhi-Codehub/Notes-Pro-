58f97109edf73a9c471cc90a6fed8e9c8e7cb507
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-15 16:54:03 +0530

    Notes Pro edit note activity

--- app/src/main/java/com/example/notespro/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notespro/NoteDetailsActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,6 +17,9 @@ import com.google.firebase.firestore.DocumentReference;
 public class NoteDetailsActivity extends AppCompatActivity {
     EditText titleEditText, contentEditText;
     ImageButton saveNoteBtn;
+    TextView pageTitleTextView;
+    String title,content,docId;
+    boolean isEditMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,24 @@ public class NoteDetailsActivity extends AppCompatActivity {
         titleEditText = findViewById(R.id.notes_title_text);
         contentEditText = findViewById(R.id.notes_content_text);
         saveNoteBtn = findViewById(R.id.save_note_btn);
+        pageTitleTextView = findViewById(R.id.page_title);
 
-        saveNoteBtn.setOnClickListener((v) -> saveNote());
+        //receive data
+        title = getIntent().getStringExtra("title");
+        content= getIntent().getStringExtra("content");
+        docId = getIntent().getStringExtra("docId");
+
+        if(docId!=null && !docId.isEmpty()){
+            isEditMode = true;
+        }
+
+        titleEditText.setText(title);
+        contentEditText.setText(content);
+        if(isEditMode){
+            pageTitleTextView.setText("Edit your note");
+           }
+
+        saveNoteBtn.setOnClickListener( (v)-> saveNote());
 
     }
 
@@ -47,7 +68,14 @@ public class NoteDetailsActivity extends AppCompatActivity {
     void saveNoteToFirebase(Note note) {
         DocumentReference documentReference;
 
+        if(isEditMode){
+            //update the note
+            documentReference = Utility.getCollectionReferenceForNotes().document(docId);
+        }else{
+            //create new note
             documentReference = Utility.getCollectionReferenceForNotes().document();
+        }
+
         documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {

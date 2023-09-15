8da7d3aca48fdb1feefa7174a2d1dc1d50504aaa
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-15 14:36:16 +0530

    Notes Pro Notes Details Activity

--- app/src/main/java/com/example/notespro/MainActivity.java
+++ app/src/main/java/com/example/notespro/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.notespro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)) );
     }
 }
---
+++ app/src/main/java/com/example/notespro/NoteDetailsActivity.java
@@ -0,0 +1,35 @@
+package com.example.notespro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+    EditText titleEditText, contentEditText;
+    ImageButton saveNoteBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+
+        saveNoteBtn.setOnClickListener((v) -> saveNote());
+
+    }
+
+    void saveNote() {
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+        if (noteTitle == null || noteTitle.isEmpty()) {
+            titleEditText.setError("Title is required");
+            return;
+        }
+
+    }
+}

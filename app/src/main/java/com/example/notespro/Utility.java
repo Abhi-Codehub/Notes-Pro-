01286f630b96acbe9f535fcceadbe9bfe88fd7c4
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-15 15:50:56 +0530

    Notes Pro Adding Recycler view

--- app/src/main/java/com/example/notespro/Utility.java
+++ app/src/main/java/com/example/notespro/Utility.java
@@ -3,10 +3,13 @@ package com.example.notespro;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class Utility {
     static void showToast(Context context, String message){
@@ -17,4 +20,9 @@ public class Utility {
         return FirebaseFirestore.getInstance().collection("notes")
                 .document(currentUser.getUid()).collection("my_notes");
     }
+    static String timestampToString(Timestamp timestamp){
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
+    }
+
 }
+
